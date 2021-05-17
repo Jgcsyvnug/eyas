1bf160582ebc72e2ca1ba15043f1d24271297f12
Jgcsyvnug <suoxiao44chg@126.com>
2021-05-17 16:40:00 +0800

    xiugai

--- eyas/src/main/java/com/hdsx/framework/json/JsonUtils.java
+++ eyas/src/main/java/com/hdsx/framework/json/JsonUtils.java
@@ -2,9 +2,8 @@ package com.hdsx.framework.json;
 
 import java.io.Writer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
